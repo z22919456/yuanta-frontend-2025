0ac4a6f8d8c3db8654ae4e3916c33b4bdf3f0c44
z01241 <z01241@lawbroker.com.tw>
2025-01-06 13:34:05 +0800

    feat: 暫持先關閉登入

--- src/app/(yuanta)/components/buttons/login-dialog.tsx
+++ src/app/(yuanta)/components/buttons/login-dialog.tsx
@@ -10,10 +10,10 @@ import {
 } from '@/components/ui/dialog';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { useState } from 'react';
-import LoginInForm, { LoginInFormData } from './login-form';
+// import LoginInForm, { LoginInFormData } from './login-form';
 
-const API_HOST =
-  process.env.NEXT_PUBLIC_API_HOST || process.env.NEXT_PUBLIC_BASE_PATH || '';
+// const API_HOST =
+//   process.env.NEXT_PUBLIC_API_HOST || process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 type Props = {
   children?: React.ReactNode;
@@ -36,28 +36,28 @@ const LoginDialog = ({ children, type }: Props) => {
   const [data, setData] = useState<TData | undefined>(undefined);
 
   // 登入API
-  async function handleLogin(values: LoginInFormData) {
-    const data = await fetch(`${API_HOST}/api/login/${type}`, {
-      method: 'POST',
-      body: JSON.stringify(values),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!data.ok) {
-      const errorBody = await data.json();
+  // async function handleLogin(values: LoginInFormData) {
+  //   const data = await fetch(`${API_HOST}/api/login/${type}`, {
+  //     method: 'POST',
+  //     body: JSON.stringify(values),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  //   if (!data.ok) {
+  //     const errorBody = await data.json();
 
-      throw new Error(errorBody.message);
-    }
-    const body = await data.json();
+  //     throw new Error(errorBody.message);
+  //   }
+  //   const body = await data.json();
 
-    if (body.success == false) {
-      throw new Error(body.message);
-    }
+  //   if (body.success == false) {
+  //     throw new Error(body.message);
+  //   }
 
-    setData(body);
-    return;
-  }
+  //   setData(body);
+  //   return;
+  // }
 
   // function handleFakeLogin() {
   //   setData({
@@ -104,14 +104,13 @@ const LoginDialog = ({ children, type }: Props) => {
           <DialogDescription className="text-yuan-blue-800 space-y-6 text-base">
             {/* h-[calc(100vh-108px)] */}
             <ScrollArea className="max-h-[calc(100vh-108px)] overflow-y-auto">
-              {/* 
               <div className="space-y-3 rounded-xl border-2 border-y-bo bg-y-card p-2 py-5 text-center">
                 <p className="text-xl font-bold">活動尚未開始</p>
                 <p className="text-xl font-bold">敬請期待</p>
-              </div> */}
-              <div className="text-center md:px-4">
-                <LoginInForm onSubmit={handleLogin} />
               </div>
+              {/* <div className="text-center md:px-4">
+                <LoginInForm onSubmit={handleLogin} />
+              </div> */}
             </ScrollArea>
           </DialogDescription>
         </DialogHeader>
