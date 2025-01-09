e00e897bcccfbd9f88e33d57d857b929e885a152
z01241 <z01241@lawbroker.com.tw>
2025-01-09 22:56:45 +0800

    feat: 更新文案

--- src/app/(yuanta)/(future-and-leverage)/future/components/five-type-dialog.tsx
+++ src/app/(yuanta)/(future-and-leverage)/future/components/five-type-dialog.tsx
@@ -21,107 +21,171 @@ const data = [
     name: '匯率',
     key: 'exchange',
     values: [
-      '澳幣(AD)',
-      '微澳幣(M6A)',
-      '英鎊(BP)',
-      '微英鎊(M6B)',
-      '加幣(CD)',
-      '微加幣(MCD)',
-      '瑞朗(SF)',
-      '歐元(EC)',
-      '小歐元(E7)',
-      '微歐元(ECM)',
-      '日元(JY)',
-      '歐元兌日元(RY)',
-      '歐元兌英鎊(UE)',
-      '紐幣(NE)',
-      '墨西哥披索(MP)',
-      '南非幣(RA)',
-      '巴西幣(BR)$',
-      '俄羅斯盧布(RU)$',
-      '盧比(SIR)$',
-      '澳幣選擇權(ADU)',
-      '英鎊選擇權(GBU)',
-      '加幣選擇權(CAU)',
-      '歐元選擇權(EUU)',
-      '日元選擇權(JPU)',
+      { name: '澳幣(AD)', price: '$ 1,485', star: false },
+      { name: '微澳幣(M6A)', price: '$ 149', star: false },
+      { name: '英鎊(BP)', price: '$ 1,925', star: false },
+      { name: '微英鎊(M6B)', price: '$ 193', star: false },
+      { name: '加幣(CD)', price: '$ 1,100', star: false },
+      { name: '微加幣(MCD)', price: '$ 110', star: false },
+      { name: '瑞朗(SF)', price: '$ 4,070', star: false },
+      { name: '歐元(EC)', price: '$ 2,530', star: false },
+      { name: '小歐元(E7)', price: '$ 1,265', star: false },
+      { name: '微歐元(ECM)', price: '$ 253', star: false },
+      { name: '日元(JY)', price: '$ 3,190', star: false },
+      { name: '歐元兌日元(RY)', price: '¥ 605,000', star: false },
+      { name: '歐元兌英鎊(UE)', price: '£ 1,870', star: false },
+      { name: '紐幣(NE)', price: '$ 1,430', star: false },
+      { name: '墨西哥披索(MP)', price: '$ 1,540', star: false },
+      { name: '南非幣(RA)', price: '$ 1,540', star: false },
+      { name: '巴西幣(BR)$', price: '$ 935', star: false },
+      { name: '俄羅斯盧布(RU)$', price: '$ 6,820', star: false },
+      { name: '盧比(SIR)$', price: '$ 770', star: false },
+      {
+        name: '澳幣選擇權(ADU)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '英鎊選擇權(GBU)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '加幣選擇權(CAU)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '歐元選擇權(EUU)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '日元選擇權(JPU)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
     ],
   },
   {
     name: '金屬',
     key: 'metal',
     values: [
-      '白金(PL)',
-      '鈀金(PA)',
-      '黃金(GC)',
-      '小黃金(QO)$',
-      '微黃金(MGC)',
-      '白銀(SI)',
-      '微白銀(SIL)',
-      '銅(HG)',
-      '微銅(MHG)$',
-      '鋁(ALI)',
-      '黃金選擇權(GC)',
-      '白銀選擇權(SI)',
+      { name: '白金(PL)', price: '$ 3,388', star: false },
+      { name: '鈀金(PA)', price: '$ 13,750', star: false },
+      { name: '黃金(GC)', price: '$ 12,650', star: false },
+      { name: '小黃金(QO)$', price: '$ 6,325', star: false },
+      { name: '微黃金(MGC)', price: '$ 1,265', star: false },
+      { name: '白銀(SI)', price: '$ 13,915', star: false },
+      { name: '微白銀(SIL)', price: '$ 2,783', star: false },
+      { name: '銅(HG)', price: '$ 6,600', star: false },
+      { name: '微銅(MHG)', price: '$ 660', star: false },
+      { name: '鋁(ALI)', price: '$ 3,850', star: false },
+      {
+        name: '黃金選擇權(GC)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '白銀選擇權(SI)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
     ],
   },
   {
     name: '指數',
     key: 'index',
     values: [
-      '小羅素2000(RTY)$',
-      '微羅素2000(M2K)$',
-      '小道瓊指數(YM)$',
-      '微道瓊指數(MYM)$',
-      '小那斯達克(NQ)$',
-      '微那斯達克(MNQ)$',
-      '小SP指數(ES)$',
-      '微SP指數(MES)$',
-      '小SP ESG指數(ESG)$',
-      '小SP衛保(XAV)$',
-      '道瓊不動產指數(RE)$',
-      '美元日經(NK)$',
-      '日元日經(NY)$',
-      '小標普400(ME)$',
-      '小道瓊指數選擇權(YM)',
-      '小SP指數選擇權(ES)',
-      '微SP指數選擇權(MES)',
-      '小那斯達克指數選擇權(NQ)',
-      '微那斯達克指數選擇權(MNQ)',
+      { name: '小羅素2000(RTY)$', price: '$ 8,360', star: false },
+      { name: '微羅素2000(M2K)$', price: '$ 836', star: false },
+      { name: '小道瓊指數(YM)$', price: '$ 11,440', star: false },
+      { name: '微道瓊指數(MYM)$', price: '$ 1,144', star: false },
+      { name: '小那斯達克(NQ)$', price: '$ 24,420', star: false },
+      { name: '微那斯達克(MNQ)$', price: '$ 2,442', star: false },
+      { name: '小SP指數(ES)$', price: '$ 16,060', star: false },
+      { name: '微SP指數(MES)$', price: '$ 1,606', star: false },
+      { name: '小SP ESG指數(ESG)$', price: '$ 10,450', star: false },
+      { name: '小SP衛保(XAV)$', price: '$ 7,370', star: false },
+      { name: '道瓊房地產(RE)$', price: '$ 2,310', star: false },
+      { name: '美元日經(NK)$', price: '$ 14,300', star: false },
+      { name: '日元日經(NY)$', price: '¥ 1,430,000', star: false },
+      { name: '小標普400(ME)$', price: '$ 20,240', star: false },
+      {
+        name: '小道瓊指數選擇權(YM)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '小SP指數選擇權(ES)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '微SP指數選擇權(MES)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '小那斯達克指數選擇權(NQ)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '微那斯達克指數選擇權(MNQ)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
     ],
   },
   {
-    name: '債券(有含利率)',
+    name: '債券(含利率)',
     key: 'bond',
     values: [
-      '美國二年債(TU)',
-      '美國五年債(FV)',
-      '美國十年債(TY)',
-      '微10年殖利率(10Y)$',
-      '美國長期十年債(UN)',
-      '美國三十年債(US)',
-      '美國超長債(UB)',
-      '30天聯邦基金(ZQ)$',
-      '一個月SOFR(SR1)$',
-      '三個月SOFR(SR3)$',
-      '美國三十年債選擇權(US)',
-      '美國十年債選擇權(TY)',
+      { name: '美國二年債(TU)', price: '$ 1,320', star: false },
+      { name: '美國五年債(FV)', price: '$ 1,430', star: false },
+      { name: '美國十年債(TY)', price: '$ 2,200', star: false },
+      { name: '微10年殖利率(10Y)$', price: '$ 352', star: false },
+      { name: '美國長期十年債(UN)', price: '$ 3,080', star: false },
+      { name: '美國三十年債(US)', price: '$ 4,070', star: false },
+      { name: '美國超長債(UB)', price: '$ 6,050', star: false },
+      { name: '30天聯邦基金(ZQ)$', price: '$ 633', star: false },
+      { name: '一個月SOFR(SR1)$', price: '$ 1,375', star: false },
+      { name: '三個月SOFR(SR3)$', price: '$ 825', star: false },
+      {
+        name: '美國三十年債選擇權(US)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '美國十年債選擇權(TY)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
     ],
   },
   {
     name: '能源',
     key: 'energy',
     values: [
-      '輕原油(CL)',
-      '小輕原油(QM)$',
-      '微輕原油(MCL)$',
-      '熱燃油(HO)',
-      '天然氣(NG)',
-      '小天然氣(QG)$',
-      '無鉛汽油(RB)',
-      'CME布侖特(BZ)$',
-      '輕原油選擇權(CL)',
-      '天然氣選擇權(NG)',
+      { name: '輕原油(CL)', price: '$ 5,720', star: false },
+      { name: '小輕原油(QM)$', price: '$ 2,860', star: false },
+      { name: '微輕原油(MCL)$', price: '$ 572', star: false },
+      { name: '熱燃油(HO)', price: '$ 6,820', star: false },
+      { name: '天然氣(NG)', price: '$ 6,050', star: false },
+      { name: '小天然氣(QG)$', price: '$ 1,513', star: false },
+      { name: '無鉛汽油(RB)', price: '$ 6,050', star: false },
+      { name: 'CME布侖特(BZ)$', price: '$ 5,170', star: false },
+      {
+        name: '輕原油選擇權(CL)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
+      {
+        name: '天然氣選擇權(NG)',
+        price: '原始保證金×1.25 ＋權利金市值',
+        star: true,
+      },
     ],
   },
 ];
@@ -156,7 +220,12 @@ const FiveTypeDialog = () => {
             {/* h-[calc(100vh-108px)] */}
             <div>
               <ScrollArea className="max-h-[calc(100vh-160px)] overflow-y-auto">
-                <div className="p-1 text-center md:px-4">
+                <div className="p-1 text-left md:px-4">
+                  <p className="text-xs">*資料日期 2025.1.9</p>
+                  <p className="text-xs">*實際保證金金額 請以交易所公告為準</p>
+                  <p className="mb-2 text-xs text-y-wall">
+                    *選擇權賣方保證金=MAX(原始保證金*1.25－1/2價外值，1/2原始保證)＋權利金市值
+                  </p>
                   <Accordion
                     type="single"
                     collapsible
@@ -173,31 +242,23 @@ const FiveTypeDialog = () => {
                         </AccordionTrigger>
                         <AccordionContent className="-mt-3 rounded-b-lg border-2 border-y-bo bg-y-card p-3 pt-6">
                           <div className="space-y-2">
-                            {item.values
-                              .reduce<string[][]>(
-                                (accumulator, currentValue, index) => {
-                                  if (index % 2 === 0) {
-                                    accumulator.push([currentValue]);
-                                  } else {
-                                    accumulator[accumulator.length - 1].push(
-                                      currentValue
-                                    );
-                                  }
-                                  return accumulator;
-                                },
-                                []
-                              )
-                              .map((value, i) => (
-                                <div
-                                  className="grid grid-cols-2 gap-2 border-b pb-2 pt-1 last-of-type:border-none"
-                                  key={i}
-                                >
-                                  <p className="text-lg">{value[0]}</p>
-                                  {value.length > 1 && (
-                                    <p className="text-lg">{value[1]}</p>
+                            {item.values.map((value, i) => (
+                              <div
+                                className="grid grid-cols-2 items-center gap-2 border-b pb-2 pt-1 last-of-type:border-none"
+                                key={i}
+                              >
+                                <p className="text-left text-lg">
+                                  {value.star && (
+                                    <span className="text-y-wall">*</span>
                                   )}
-                                </div>
-                              ))}
+                                  {value.name}
+                                </p>
+
+                                <p className="text-right text-base">
+                                  {value.price}
+                                </p>
+                              </div>
+                            ))}
                           </div>
                         </AccordionContent>
                       </AccordionItem>
